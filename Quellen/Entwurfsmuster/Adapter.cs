f079b2abeb7c9f049618a94430a44bf24529a251
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2025-05-16 18:33:36 +0200

    add code examples and more illustrations for design patterns

---
+++ Quellen/Entwurfsmuster/Adapter.cs
@@ -0,0 +1,42 @@
+﻿namespace Entwurfsmuster
+{
+    public interface AllgemeineSchnittstelle
+    {
+        public int Dienst(int a, int b);
+    }
+
+    public class AdapterSumme : AllgemeineSchnittstelle
+    {
+        public int Dienst(int a, int b)
+        {
+            // Übersetzung der allgemeinen Schnittstelle auf den Addierer
+            return new Addierer().Addiere(a, b);
+        }
+    }
+
+    public class AdapterProdukt : AllgemeineSchnittstelle
+    {
+        public int Dienst(int a, int b)
+        {
+            // Übersetzung der allgemeinen Schnittstelle auf den Multiplizierer
+            return new Multiplizierer().Multipliziere(a, b);
+        }
+    }
+
+    public class Addierer
+    {
+        public int Addiere(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class Multiplizierer
+    {
+        public int Multipliziere(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
+
---
+++ Quellen/Entwurfsmuster/Proxy.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace Entwurfsmuster
+{
+    public interface ProxySchnittstelle
+    {
+        Task<int> Dienst(int a, int b);
+    }
+
+    public class InputNachricht
+    {
+        public int A;
+        public int B;
+    }
+    public class OutputNachricht
+    {
+        public int R;
+    }
+
+    public class Proxy : ProxySchnittstelle
+    {
+        private HttpClient client = new HttpClient();
+
+        private string url = "https://domain/pfad/";
+
+        public async Task<int> Dienst(int a, int b)
+        {
+            // InputNachricht erstellen und in JSON-Darstellung konvertieren
+            var input = new InputNachricht { A = a, B = b };
+            var inputJson = JsonSerializer.Serialize(input);
+            var inputContent = new StringContent(inputJson);
+
+            // InputNachricht über HTTP an Dienst-Implementierung senden und Antwort empfangen
+            var antwort = await client.PostAsync(url, inputContent);
+
+            // JSON-Darstellung in OutputNachricht konvertieren und Rückgabewert auslesen
+            var outputJson = await antwort.Content.ReadAsStringAsync();
+            var output = JsonSerializer.Deserialize<OutputNachricht>(outputJson);
+            return output != null ? output.R : 0;
+        }
+    }
+}
+
